80a212a9c31c436af884fade9ae4cbb0b71e5718
Shraddha <Shraddha@DESKTOP-PHLA91I>
2023-08-07 08:56:05 -0700

    Add project files.

---
+++ sbExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using sbExpenseTracker.Models;
+
+namespace sbExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
