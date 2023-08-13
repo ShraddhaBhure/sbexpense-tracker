9c50ebfa5a90fc70a4e8a6811acb5e0dc9ebfa14
Shraddha <Shraddha@DESKTOP-PHLA91I>
2023-08-13 18:28:37 +0530

    Transaction complete

---
+++ sbExpenseTracker/Controllers/TransactionController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using sbExpenseTracker.Data;
+using sbExpenseTracker.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sbExpenseTracker.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+
+            var applicationDbContext = _context.Transactions.Include(t => t.Category);
+           
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id =0)
+        {
+            PopulateCaegories();
+            if(id == 0)
+            return View(new Transaction());
+            else
+                return View(_context.Transactions.Find(id));
+
+        }
+
+        // POST: Transaction/AddOrEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if(transaction.TransactionId==0)
+                _context.Add(transaction);
+                else
+                    _context.Update(transaction);
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ////   ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
+            PopulateCaegories();
+            return View(transaction);
+        }
+
+
+      
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Transactions == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Categories'  is null.");
+            }
+            var transactions = await _context.Transactions.FindAsync(id);
+            if (transactions != null)
+            {
+                _context.Transactions.Remove(transactions);
+            }
+           
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TransactionId == id);
+        }
+
+        [NonAction]
+        public void PopulateCaegories()
+        {
+            var CategoryCollection = _context.Categories.ToList();
+            Category DefaultCategory = new Category() { CategoryId = 0, Title = "Choose a Category" };
+            CategoryCollection.Insert(0, DefaultCategory);
+            ViewBag.Categories = CategoryCollection;    
+        
+        }
+    }
+}
--- sbExpenseTracker/Models/Transaction.cs
+++ sbExpenseTracker/Models/Transaction.cs
@@ -8,12 +8,37 @@ namespace sbExpenseTracker.Models
     {
         [Key]
         public int TransactionId { get; set; }
-        //CategoryId
+        
+        [Range(1,int.MaxValue,ErrorMessage ="Please Select a category.")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; }  //to specify foreign key
+
+        public Category? Category { get; set; }  //to specify foreign key//? means validations property value can be nullable 
+       
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
+
         [Column(TypeName = "nvarchar(50)")]
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
-    }
+        [NotMapped]
+        public string CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + "" + Category.Title;
+
+            }
+        }
+
+        [NotMapped]
+        public string FormatedAmount
+        {
+            get
+            {
+                return ((Category == null|| Category.Type == "Expense" ) ? "- " : "+ " ) + Amount.ToString("C0");
+
+            }
+
+        }
+     }
 }
